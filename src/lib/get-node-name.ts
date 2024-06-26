95214e2a3f07856dee7483bbf92a7ec0266b630d
Tomas Hubelbauer <tomas@hubelbauer.net>
2024-06-26 17:32:58 +0200

    Fail on error, support array operations, CompositeTypes, quoted identifiers and single-member enums
    This makes the library capable of handling the TypeScript types as generated by recent versions of the Supabase CLI as well as makes it able to cover some more exotic scenarios like the aforementioned single-member enums and quoted identifiers.

--- src/lib/get-node-name.ts
+++ src/lib/get-node-name.ts
@@ -6,6 +6,11 @@ export const getNodeName = (n: ts.Node) => {
     if (ts.isIdentifier(n)) {
       name = n.text;
     }
+
+    // Handle quoted identifiers in case they contain special characters
+    if (ts.isStringLiteral(n)) {
+      name = n.text;
+    }
   });
   if (!name) throw new Error('Cannot get name of node');
   return name;
